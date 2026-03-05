b729d03993c6657ea04514cee0374d8fe89b3c7b
DavidC41 <davidcamposgarrido41@gmail.com>
2026-03-05 18:41:50 +0100

    Uso de Introduce Parameter Object en método validar

--- src/Grupo5/FormValidator.java
+++ src/Grupo5/FormValidator.java
@@ -5,15 +5,16 @@ public class FormValidator {
 	final int TAMAÑOTEL = 9;
 	final int TAMAÑOCP= 5;
 	
-    public boolean validar(String nombre, String email, String telefono, 
-            String direccion, String ciudad, String codigoPostal) {
+    public boolean validar(ValidarParameter parameterObject) {
 
-    	if (nombre == null || nombre.isEmpty()) return false;
-		if (email == null || !email.contains("@")) return false;
-		if (telefono == null || telefono.length() < TAMAÑOTEL) return false;
-		if (direccion == null || direccion.isEmpty()) return false;
-		if (ciudad == null || ciudad.isEmpty()) return false;
-		if (codigoPostal == null || codigoPostal.length() != TAMAÑOCP ) return false;
+    	if (parameterObject.nombre == null || parameterObject.nombre.isEmpty()) return false;
+		if (parameterObject.email == null || !parameterObject.email.contains("@")) return false;
+		if (parameterObject.telefono == null || parameterObject.telefono.length() < TAMAÑOTEL) return false;
+		if (parameterObject.direccion == null || parameterObject.direccion.isEmpty()) return false;
+		if (parameterObject.ciudad == null || parameterObject.ciudad.isEmpty()) return false;
+		if (parameterObject.codigoPostal == null || parameterObject.codigoPostal.length() != TAMAÑOCP ) return false;
 		return true;
+		
+		
 	}
 }
