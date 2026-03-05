eb6750f6d30ac756425f4480ab57493e0a532b59
DavidC41 <davidcamposgarrido41@gmail.com>
2026-03-05 18:28:43 +0100

    Modificación del código agregando constantes

--- src/Grupo5/FormValidator.java
+++ src/Grupo5/FormValidator.java
@@ -2,15 +2,18 @@ package Grupo5;
 
 public class FormValidator {
 
+	final int TAMAÑOTEL = 9;
+	final int TAMAÑOCP= 5;
+	
     public boolean validar(String nombre, String email, String telefono, 
             String direccion, String ciudad, String codigoPostal) {
 
     	if (nombre == null || nombre.isEmpty()) return false;
 		if (email == null || !email.contains("@")) return false;
-		if (telefono == null || telefono.length() < 9) return false;
+		if (telefono == null || telefono.length() < TAMAÑOTEL) return false;
 		if (direccion == null || direccion.isEmpty()) return false;
 		if (ciudad == null || ciudad.isEmpty()) return false;
-		if (codigoPostal == null || codigoPostal.length() != 5) return false;
+		if (codigoPostal == null || codigoPostal.length() != TAMAÑOCP ) return false;
 		return true;
 	}
 }
